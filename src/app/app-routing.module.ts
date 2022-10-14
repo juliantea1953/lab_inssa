3eb4d22821e1f8bbd6bbb947c4d3f4edd48b73c9
davidtea <davidtea1953@gmail.com>
2022-10-14 10:30:57 -0500

    Se genero la vista del login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LogComponent } from './log/log.component';
 
 const routes: Routes = [];
 
